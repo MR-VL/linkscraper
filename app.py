0f9545f01fb4738368cef4f1f9b72ae537022db7
mickey <cosmmo6@gmail.com>
2024-06-26 19:50:32 -0500

    Initial commit everything works from here

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ import pandas as pd
 app = Flask(__name__)
 urls = []
 
-@app.route('/')
-def redirect_home():
-    return redirect('/home')
-
 @app.route('/linkscraper')
 def index():
     return render_template('index.html')
@@ -19,29 +15,38 @@ def index():
 def home():
     return render_template('home.html')
 
-@app.route('/embed', methods=['GET', 'POST'])
-def embed():
-    if request.method == 'POST':
-        return render_template('embedd.html')
+@app.route('/')
+def reroutehome():
+    return redirect('home')
+@app.route('/embed')
+def embedScrape():
     return render_template('embed.html')
+
 
 def htmlDoc(url):
     page = requests.get(url)
     return page.text
 
+
 def scrape_and_save_urls(url, type):
     try:
-        urls.clear()  # Clear previous URLs before scraping
         html = htmlDoc(url)
         soup = bs4(html, 'html.parser')
+
 
         for link in soup.findAll('a', attrs={type: re.compile('^https://')}):
             urls.append(link.get('href'))
 
+
         return urls  # Return the list of URLs instead of just the filename
     except Exception as e:
         error = f"ERROR: Scraper ran into an issue: {e}. Please try again, make sure CSV is not open during the scraping process."
+
         return error
+
+
+
+
 
 @app.route('/scrape', methods=['POST'])
 def scrapeLinks():
@@ -52,32 +57,15 @@ def scrapeLinks():
     else:
         return render_template('index.html', error=scraped_urls)  # Pass error message to the template
 
+
 @app.route('/download', methods=['POST'])
 def download():
     df = pd.DataFrame(urls, columns=['URL'])
     df.to_csv('scrapedURLS.csv', index=False)
     return send_file('scrapedURLS.csv', as_attachment=True)
 
-def extract_video_url(embed_url):
-    response = requests.get(embed_url)
-    soup = bs4(response.content, 'html.parser')
-    iframe_src = soup.find('iframe')['src']
-    return iframe_src
 
-@app.route('/embedd', methods=['POST'])
-def embeddownload():
-    link = request.form['url']
-    vidurl = extract_video_url(link)
-    response = requests.get(vidurl)
 
-    if response.status_code == 200:
-        filename = 'downloaded_video.mp4'
-        with open(filename, 'wb') as f:
-            f.write(response.content)
-        print(f"Video downloaded successfully as '{filename}'")
-        return f"Video downloaded successfully as '{filename}'"
-    else:
-        return "Failed to download the video"
 
 if __name__ == "__main__":
     app.run(debug=True)
