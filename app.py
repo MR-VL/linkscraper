0219408b6b1912265f60a7dc8ffddc5954be111f
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-06-30 15:07:27 -0500

    push changed

--- app.py
+++ app.py
@@ -3,21 +3,32 @@ from bs4 import BeautifulSoup as bs4
 import requests
 import re
 import pandas as pd
+from pytube import YouTube
 
 app = Flask(__name__)
 urls = []
+
 
 @app.route('/linkscraper')
 def index():
     return render_template('index.html')
 
+
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+
+@app.route('/cookie')
+def cookie():
+    return render_template('cookie.html')
+
+
 @app.route('/')
 def reroutehome():
     return redirect('home')
+
+
 @app.route('/embed')
 def embedScrape():
     return render_template('embed.html')
@@ -33,19 +44,14 @@ def scrape_and_save_urls(url, type):
         html = htmlDoc(url)
         soup = bs4(html, 'html.parser')
 
-
         for link in soup.findAll('a', attrs={type: re.compile('^https://')}):
             urls.append(link.get('href'))
-
 
         return urls  # Return the list of URLs instead of just the filename
     except Exception as e:
         error = f"ERROR: Scraper ran into an issue: {e}. Please try again, make sure CSV is not open during the scraping process."
 
         return error
-
-
-
 
 
 @app.route('/scrape', methods=['POST'])
@@ -64,6 +70,45 @@ def download():
     df.to_csv('scrapedURLS.csv', index=False)
     return send_file('scrapedURLS.csv', as_attachment=True)
 
+
+
+
+
+@app.route('/cookieScrape', methods=['POST'])
+def cookieScrape():
+    url = request.form['url']
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
+        cookies = response.cookies.get_dict()
+        if cookies:
+            return render_template('cookie.html', cookies=cookies, url=url)
+        else:
+            return render_template('cookie.html', error="No cookies found")
+    except requests.exceptions.RequestException as e:
+        return render_template('cookie.html', error=f"Error occurred: {e}")
+
+
+@app.route('/youtube')
+def yt():
+    return render_template('youtube.html')
+
+@app.route('/ytDownload', methods = ['POST'])
+def ytDownload():
+    try:
+        url = request.form['url']
+        yt = YouTube(url)
+        video = yt.streams.get_highest_resolution()
+        if video:
+            video_path = video.download()
+            return send_file(video_path, as_attachment=True)
+
+        else:
+            return render_template('youtube.html', error="No video found")
+    except requests.exceptions.RequestException as e:
+        return render_template('youtube.html', error=f"Error occurred: {e}")
+    except requests.exceptions.RequestException as e:
+        return render_template('youtube.html', error=f"Error occurred: {e}")
 
 
 
